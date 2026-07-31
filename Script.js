c5fee00f1f36bfd4513abf7efe3724c14535ce9c
dariobuti02 <dario.buti02@gmail.com>
2026-07-31 21:11:11 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -16,6 +16,30 @@ const firebaseConfig = {
   appId: "1:1004654981773:web:4567b072b212592ad368c4"
 };
 
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCNlLl9cp7m0ZjlsLWBGmsq5XW8cjIYcGU",
+    authDomain: "opsnet-801c4.firebaseapp.com",
+    databaseURL: "https://opsnet-801c4-default-rtdb.firebaseio.com",
+    projectId: "opsnet-801c4",
+    storageBucket: "opsnet-801c4.firebasestorage.app",
+    messagingSenderId: "1004654981773",
+    appId: "1:1004654981773:web:4567b072b212592ad368c4",
+    measurementId: "G-V0TEDD1WQM"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
 const PASSWORD = "Opsnet";
 
 
