e87f4ef6db3cd2c51b0145036fd561a2364937a8
dariobuti02 <dario.buti02@gmail.com>
2026-07-31 17:56:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,89 @@
-// Cambia questa password con quella che vuoi usare
+// ================================
+// OPSNET - Gestione accesso
+// ================================
+
 const PASSWORD = "Opsnet";
 
+
+// Login principale
 function login() {
 
     const passwordInserita = document.getElementById("password").value;
     const errore = document.getElementById("errore");
 
     if (passwordInserita === PASSWORD) {
+
+        // Accesso al portale
         window.location.href = "portal.html";
+
     } else {
+
         errore.textContent = "Password non corretta";
+
     }
 
 }
 
 
+// ================================
+// Apertura settori OPSNET
+// ================================
+
 function apriSettore(settore) {
 
-    if (settore === "vvf") {
-        window.location.href = "vvf.html";
+    switch (settore) {
+
+        case "vvf":
+            window.location.href = "vvf.html";
+            break;
+
+        case "polizia":
+            window.location.href = "polizia.html";
+            break;
+
+        case "carabinieri":
+            window.location.href = "carabinieri.html";
+            break;
+
+        case "protezione":
+            window.location.href = "protezione_civile.html";
+            break;
+
+        case "copass":
+            window.location.href = "copass.html";
+            break;
+
+        case "aib":
+            window.location.href = "aib.html";
+            break;
+
+        case "aerea":
+            window.location.href = "banda_aerea.html";
+            break;
+
+        case "pmr":
+            window.location.href = "pmr446.html";
+            break;
+
+        case "lpd":
+            window.location.href = "lpd.html";
+            break;
+
+        case "radioamatori":
+            window.location.href = "radioamatori.html";
+            break;
+
+        case "nautica":
+            window.location.href = "nautica.html";
+            break;
+
+        case "118":
+            window.location.href = "118.html";
+            break;
+
+        default:
+            alert("Settore non disponibile");
+
     }
 
 }
